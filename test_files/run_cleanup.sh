a515dce393a26765f9f9d120a1fbe188a06f0fda
Jonathan Hornung <jahornung@gmail.com>
2016-11-21 09:48:28 -0600

    Add working branch test to cleanup script

--- test_files/run_cleanup.sh
+++ test_files/run_cleanup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "This will delete files in the current folder that do not have valid json"
+echo "This shouldn't be needed to run the script much longer - close to getting proper error detection"
+echo "Will continue in 10 seconds"
+sleep 10
+
 for i in `ls` ; do
 	pushd $i
 	python ../../test_files/verify_json.py
